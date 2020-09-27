9d3ec725d6c42e1a9f96d0b1830f349e2783bc1c
Song Wook <songtri@gmail.com>
2020-09-27 02:55:38 -0400

    activity, fragment, string setup except view activity

---
+++ app/src/main/java/com/example/wooksong_comp304_lab1_ex1/AIActivity.java
@@ -0,0 +1,53 @@
+package com.example.wooksong_comp304_lab1_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AIActivity extends AppCompatActivity {
+
+    private TextView textStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai);
+        textStatus = (TextView)findViewById(R.id.text_activity_ai_status);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        textStatus.setText(R.string.lifecycle_status_onstart);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        textStatus.setText(R.string.lifecycle_status_onrestart);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        textStatus.setText(R.string.lifecycle_status_onresume);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        textStatus.setText(R.string.lifecycle_status_onpause);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        textStatus.setText(R.string.lifecycle_status_onstop);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textStatus.setText(R.string.lifecycle_status_ondestroy);
+    }
+}
---
+++ app/src/main/java/com/example/wooksong_comp304_lab1_ex1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.wooksong_comp304_lab1_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment bottomFragment;
+    private TextView textStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomFragment = getSupportFragmentManager().findFragmentById(R.id.bottomFragment);
+        textStatus = (TextView)findViewById(R.id.text_activity_main_status);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        textStatus.setText(R.string.lifecycle_status_onstart);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        textStatus.setText(R.string.lifecycle_status_onrestart);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        textStatus.setText(R.string.lifecycle_status_onresume);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        textStatus.setText(R.string.lifecycle_status_onpause);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        textStatus.setText(R.string.lifecycle_status_onstop);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textStatus.setText(R.string.lifecycle_status_ondestroy);
+    }
+}
